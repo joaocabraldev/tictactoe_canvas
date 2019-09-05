b0487a3958e62d89730aa6fabb0477295416f8f2
Joao Antonio <joaoantonio@Megasoft.local>
2019-09-05 13:35:08 -0300

    Opções do Jogo

--- public/js/game.js
+++ public/js/game.js
@@ -20,16 +20,31 @@ const view = {
         [25, 140], [90, 140], [160, 140],
         [25, 210], [90, 210], [160, 210]
     ],
+    
+    options: ['X', 'O'],
+    turn: 0,
+    board: ['','','','','','','','',''],
+	winning_sequences: [
+		[0,1,2], [3,4,5], [6,7,8],
+		[0,3,6], [1,4,7], [2,5,8],
+		[0,4,8], [2,4,6]
+	],
+	game_over: false,
 
     init() {
         this.canvas = document.getElementById('canvas');
         this.canvas.addEventListener('click', this.draw_play, true);
+        
+        this.btn_reset = document.getElementById('reset');
+        //this.canvas.addEventListener('click', this.reset_board, true);
+        
         this.context = this.canvas.getContext('2d');
         this.context.font = '48px serif';
         this.draw_board();
     },
 
     draw_board() {
+        this.context.clearRect(0,0,230,230);
         let h = this.dimensions.height;
         let w = this.dimensions.width;
 
@@ -42,17 +57,20 @@ const view = {
     },
 
     draw_play(evt) {
-        let x = evt.clientX - canvas.offsetLeft;
-        let y = evt.clientY - canvas.offsetTop;
+        if (!view.game_over) {
+            let x = evt.clientX - canvas.offsetLeft;
+            let y = evt.clientY - canvas.offsetTop;
+            let cell = view.check_cell(x, y);
 
-        console.clear();
-        console.log("Posição do clique: x:", x + ", y: " + y);
-        let cell = view.check_cell(x, y);
-        console.log("Clicou na célula " + cell + "!");
-        view.write_cell(cell);
-        view.context.fillText('X', cell[0], cell[1]);
+            view.write_cell(cell);
+            view.context.fillText(view.options[view.turn], cell[0], cell[1]);
+            
+            view.fill_board(cell);
+            view.check_winner(view.options[view.turn]);
+            view.change_player();
+        }
     },
-
+    
     check_cell(x, y) {
         for (let i = 0; i < this.areas.length; i++) {
             const p = this.areas[i];
@@ -66,76 +84,50 @@ const view = {
         let pos = cell - 1;
         let x = this.text_positions[pos][0];
         let y = this.text_positions[pos][1];
-        this.context.fillText('X', x, y);
-    }
-}
-
-
-/*
-function draw() {
-    let canvas = document.getElementById('tutorial');
-    canvas.addEventListener('click', clicked, true);
-
-    if (canvas.getContext){
-        let ctx = canvas.getContext('2d');
-        
-        //ctx.strokeRect(10, 10, 80, 80);
-
-        /*
-        ctx.strokeStyle = '#000000';
-        ctx.strokeRect(10, 10, 70, 70);
-        ctx.strokeRect(10, 10, 70, 70);
-
-        ctx.font = '48px serif';
-        ctx.fillText('X', 25, 60);
-
-        ctx.strokeRect(80, 10, 70, 70);
-        ctx.strokeRect(80, 10, 70, 70);
-
-        ctx.strokeRect(150, 10, 70, 70);
-        ctx.strokeRect(150, 10, 70, 70);
-
-
-        ctx.strokeRect(10, 80, 70, 70);
-        ctx.strokeRect(10, 80, 70, 70);
-
-        ctx.strokeRect(80, 80, 70, 70);
-        ctx.strokeRect(80, 80, 70, 70);
-        
-        ctx.strokeRect(150, 80, 70, 70);
-        ctx.strokeRect(150, 80, 70, 70);
-
-
-        ctx.strokeRect(10, 150, 70, 70);
-        ctx.strokeRect(10, 150, 70, 70);
-
-        ctx.strokeRect(80, 150, 70, 70);
-        ctx.strokeRect(80, 150, 70, 70);
-        
-        ctx.strokeRect(150, 150, 70, 70);
-        ctx.strokeRect(150, 150, 70, 70);
-        
-    }
-}
-
-function clicked(evt) {
-
-    let canvas = document.getElementById("tutorial");
-
-    // posição dos cliques descontando a distância da borda da janela
-    let x = evt.clientX - canvas.offsetLeft;
-    let y = evt.clientY - canvas.offsetTop;
-
-    console.clear();
-    console.log("Posição do clique: x:", x + ", y:" + y);
-
-    for (let i = 0; i < view.positions.length; i++) {
-        const p = view.positions[i];
-        if (x > p[0] && x < p[1] && y > p[2] && y < p[3]) {
-            let cell = i + 1;
-            console.log("Clicou na célula " + (i+1) + "!");
+        this.context.fillText(this.options[this.turn], x, y);
+    },
+    
+    fill_board(index) {
+        this.board[index-1] = this.options[this.turn];
+        console.log(this.board);
+    },
+    
+    change_player() {
+        this.turn = (this.turn === 0 ? 1 : 0);
+    },
+    
+    check_winner(symbol) {
+        if (!this.board.includes('')) {
+            this.game_over = true;
+            console.log('It is a draw!');
         }
-    }
 
+		for (i in this.winning_sequences) {
+			if(this.board[this.winning_sequences[i][0]] == symbol &&
+				this.board[this.winning_sequences[i][1]] == symbol &&
+				this.board[this.winning_sequences[i][2]] == symbol) {
+				
+				this.game_over = true;
+                this.fill_winner(i);
+				console.log(this.winning_sequences[i]);
+                console.log('Player ' + symbol +  ' won!');
+			
+			}
+		}
+    },
+    
+    fill_winner(index) {
+        let a_index = this.winning_sequences[index][0];
+        let b_index = this.winning_sequences[index][1];
+        let c_index = this.winning_sequences[index][2];
+        
+        /*
+        TODO: Learn how to fill rectangles or redraw them.
+        let elements = document.querySelectorAll('.cell');
+
+        elements[a_index].classList.add('winner');
+        elements[b_index].classList.add('winner');
+        elements[c_index].classList.add('winner');
+        */
+    }
 }
-*/
